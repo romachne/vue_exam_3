52df9e3066ea07a16b4224c537bebfc56aafbaaf
Roma <romachne@gmail.com>
2022-04-18 17:55:06 +0300

    gotovo

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <about-page />
+</template>
+
+<script>
+import AboutPage from "@/components/AboutPage";
+
+export default {
+  name: 'AboutView',
+
+  components: {
+    AboutPage
+  },
+}
+</script>
